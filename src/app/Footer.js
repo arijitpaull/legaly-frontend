9b9c9e26359c86b19d2c8e546eb550ef65738d28
Arijit Paul <48390388+arijitpaull@users.noreply.github.com>
2025-03-10 15:54:28 +0530

    Update Footer.js

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,7 +1,7 @@
 
 const Footer = () => (
     <footer className="text-sm bg-white-600 text-white p-3 mt-2 text-center">
-      <p>&copy; {new Date().getFullYear()} Legaly - Legal Chatbot by Arijit Paul.</p>
+      <p>&copy; {new Date().getFullYear()} Legaly - Legal Chatbot.</p>
     </footer>
   );
   
